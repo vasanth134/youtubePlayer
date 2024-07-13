e45804da8c4b316d784d05585e7e2f61aded542d
vasanth134 <crazyvasanth004@gmail.com>
2024-07-13 22:04:08 +0530

    chore: Add search functionality to YouTube API Example

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const videoCardContainer =document.querySelector('.videoCard');
+const videoCardContainer = document.querySelector(".videoCard");
 
 let api_key = "AIzaSyAchR6Bv7zMdJjT3ayisyW4nrWFBhSNiAA";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+let search_http = "https://www.googleapis.com/youtube/v3/search?";
+
+// Fetch most popular videos initially
 fetch(
   video_http +
     new URLSearchParams({
       key: api_key,
       part: "snippet",
       chart: "mostPopular",
-      maxResults: 1,
+      maxResults: 100,
       regionCode: "IN",
     })
 )
   .then((res) => res.json())
-  .then(data => {
-    //   console.log(data);
-    data.items.forEach(item => {
+  .then((data) => {
+    data.items.forEach((item) => {
       getChannelIcon(item);
-    })
+    });
   })
   .catch((err) => console.log(err));
 
@@ -31,27 +33,59 @@ const getChannelIcon = (video_data) => {
         id: video_data.snippet.channelId,
       })
   )
-  .then(res => res.json())
-  .then(data => {
-    video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-    // makeVideoCard(video_data);
-})
+    .then((res) => res.json())
+    .then((data) => {
+      video_data.channelThumbnail =
+        data.items[0].snippet.thumbnails.default.url;
+      makeVideoCard(video_data);
+    });
 };
-
 
 const makeVideoCard = (data) => {
-    videoCardContainer.innerHTML +=`
-  <div class="video" onclick="location.href = 'https://www.youtube.com/watch?v=${data.id}'>
-<img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
-<div class="content">
-<img src="${data.channelThumbnail}" class="channel-icon" alt="">
-<div class="info">
-<h4 class="title">${data.snippet.title}</h4>
-<p class="channel-name"${data.snippet.channelTitle}></p>
-</div>
-</div>
-</div>
-    `
-
+  // Handle both types of data structures: popular videos and search results
+  const videoId = data.id.videoId ? data.id.videoId : data.id;
+  
+  videoCardContainer.innerHTML += `
+  <div class="video" onclick="location.href='https://www.youtube.com/watch?v=${videoId}'">
+    <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+    <div class="content">
+      <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+      <div class="info">
+        <h4 class="title">${data.snippet.title}</h4>
+        <p class="channel-name">${data.snippet.channelTitle}</p>
+      </div>
+    </div>
+  </div>
+  `;
 };
 
+const searchInput = document.querySelector(".searchInput");
+const searchBtn = document.querySelector(".searchSubmitButton");
+
+const fetchVideos = (query) => {
+  fetch(
+    search_http +
+      new URLSearchParams({
+        key: api_key,
+        part: "snippet",
+        q: query,
+        maxResults: 20,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      videoCardContainer.innerHTML = ''; // Clear previous results
+      data.items.forEach((item) => {
+        if (item.id.kind === "youtube#video") {
+          getChannelIcon(item);
+        }
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value.length) {
+    fetchVideos(searchInput.value);
+  }
+});
